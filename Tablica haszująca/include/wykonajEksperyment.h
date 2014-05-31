96d7b43efbf9e3606a496b906e41b4260af7f4d3
adamdabrowski04 <184202@student.pwr.edu.pl>
2014-05-31 22:44:55 +0200

    tablic haszujaca

---
+++ Tablica haszująca/include/wykonajEksperyment.h
@@ -0,0 +1,73 @@
+#ifndef WYKONAJEKSPERYMENT_H
+#define WYKONAJEKSPERYMENT_H
+/*!
+ *\file
+ *\brief Definicja klasy wykonajEksperyment
+ *
+ *Plik zawiera definicję klasy wykonajEksperyment, która
+ *jest odpowiedzialna za policzenie statystyk dla wybranej
+ *liczby realizacji badanego algorytmu
+ *
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<chrono>
+#include "../include/TabblicaAsocjacyjna.h"
+#include "../src/TabblicaAsocjacyjna.cpp"
+//#include "../include/Element.h"
+//#include "../src/Element.cpp"
+
+
+
+class wykonajEksperyment
+{
+    enum TypFunkcji{WCZYTYWANIE, WYSZUKIWANIE};
+    public:
+    const kolejkaCzyStos wyborPojemnika = WCZYTYWANIE;
+    const unsigned int  rozmiarTabIlosciDanych=5;
+    const unsigned int liczbaWywolan=300;
+    unsigned int *pTabIloscDanych;
+    long double *pTabCzasyKolejnychSeriiDanych;
+
+    void test();
+
+    /*!
+     *\brief wywoluje kilkukrotnie funkcje badajaca czas trwania badanego algorytmu
+     *
+     *Wywoluje kilkukrotnie funkcje mierzaca czas trwania badanego algorytmu po czym zwraca sredni czas trwania
+     *
+     *ARGUMENTY: liczbaWywolan - liczba wywolan badanej funkcji( mierzony jest czas pojedynczego wywolania)
+     *           iloscDanych   - ilosc danych kladzionych na stos\kolejke
+     */
+    unsigned int seriaPomiarow(unsigned int liczbaWywolan, unsigned int iloscDanych);
+    /*!
+     *\brief Wywałanie rozwiązania aktualnie badanego problemu
+     *
+     *PARAMETRY: liczbaDanych - liczba danych dla jakich badamy problem, w tym wypadku jest to liczba elementów kładzionych na stos lub kolejkę
+     */
+    void Wczytywanie(unsigned int liczbaDanych);
+        /*!
+     *\brief Wywałanie rozwiązania aktualnie badanego problemu
+     *
+     *PARAMETRY: liczbaDanych - liczba danych dla jakich badamy problem, w tym wypadku jest to liczba elementów kładzionych na stos lub kolejkę
+     */
+    void uruchomStos(unsigned int liczbaDanych);
+    /*!
+     *
+     * Alokuje pamiec i inicjalizuje tabelę pTabIloscDanych kolejnymi potęgami 10 przy czym pierwszy element=50
+     *
+     */
+    wykonajEksperyment();
+        ~wykonajEksperyment();
+//    void wygenerujDaneTestowe();
+    double zmierzCzasTrwania(unsigned int);
+
+    protected:
+    private:
+};
+template<class TYP>
+TYP potega(TYP, unsigned int );
+#endif // WYKONAJEKSPERYMENT_H
---
+++ Tablica haszująca/src/wykonajEksperyment.cpp
@@ -0,0 +1,150 @@
+#include "../include/wykonajEksperyment.h"
+
+using namespace std;
+wykonajEksperyment::wykonajEksperyment()
+{
+    pTabCzasyKolejnychSeriiDanych=new  long double[rozmiarTabIlosciDanych];
+    pTabIloscDanych= new unsigned  int[rozmiarTabIlosciDanych];
+    for(unsigned int i=0;i<rozmiarTabIlosciDanych;i++)
+        pTabIloscDanych[i]=potega(10, i);
+
+}
+
+wykonajEksperyment::~wykonajEksperyment()
+{
+    //dtor
+}
+
+void wykonajEksperyment::test()
+{
+    ofstream oStrm;
+    string nazwaPliku;
+        switch(wyborPojemnika)
+    {
+    case WCZYTYWANIE:
+       nazwaPliku="Wczytywanie_";
+       break;
+    case WYSZUKIWANIE:
+         nazwaPliku="Wyszukiwanie_";
+        break;
+    default:
+        cerr<<"Nie istnieje taka funkcja do sprawdzenia"<<endl;
+        break;
+    }
+    nazwaPliku+="IlosciDanych10^";
+    nazwaPliku+=to_string(rozmiarTabIlosciDanych);
+    nazwaPliku+="_liczbaWywolan_";
+    nazwaPliku+=to_string(liczbaWywolan);
+    nazwaPliku+=".csv";
+    oStrm.open(nazwaPliku);
+    for(unsigned int i=0; i<rozmiarTabIlosciDanych;i++)
+    {
+        pTabCzasyKolejnychSeriiDanych[i]=this->seriaPomiarow(liczbaWywolan, pTabIloscDanych[i] );
+        //oStrm.width(10);
+        oStrm<<pTabIloscDanych[i]<<" "<<pTabCzasyKolejnychSeriiDanych[i]<<endl;
+        cout.width(10);
+        cout<<pTabIloscDanych[i]<<"  "<<pTabCzasyKolejnychSeriiDanych[i]<<endl;
+    }
+    oStrm.close();
+}
+
+
+unsigned int wykonajEksperyment::seriaPomiarow(unsigned int liczbaWywolan, unsigned int iloscDanych)
+{
+    long double sredniCzas=0;
+    for(unsigned int i=0; i<liczbaWywolan; i++)
+    {
+        sredniCzas+=this->zmierzCzasTrwania(iloscDanych)/liczbaWywolan;
+    }
+    return sredniCzas;
+}
+
+double wykonajEksperyment::zmierzCzasTrwania(unsigned int iloscDanych)
+{
+    typedef std::chrono::high_resolution_clock high_resolution_clock;
+    typedef std::chrono::nanoseconds nanoseconds;
+    high_resolution_clock::time_point start, koniec;
+    nanoseconds czasTrwaniaObliczen;
+    start=high_resolution_clock::now();
+    switch(wyborPojemnika)
+    {
+    case WCZYTYWANIE:
+       uruchomKolejka(iloscDanych);
+       break;
+    case WYSZUKIWANIE:
+         uruchomStos(iloscDanych);
+        break;
+    default:
+        cerr<<"Nie istnieje taka funkcja do sprawdzenia"<<endl;
+        break;
+    }
+    koniec=high_resolution_clock::now();
+    czasTrwaniaObliczen=std::chrono::duration_cast<nanoseconds>(koniec - start);
+    return czasTrwaniaObliczen.count();
+}
+
+void wykonajEksperyment::Wczytytwanie(unsigned int iloscDanych)
+{
+    ifstream StrmWe;
+    StrmWe.open("baza_danych.txt")
+    string slowo;
+        Tablica_Haszujaca<string, int> slownik();
+    for(unsigned int i=0; i<iloscDanych;i++)
+    {
+        StrnWe>>slowo;
+        slownik.dodaj(slowo,i);
+    }
+
+    StrmWe.close();
+}
+
+//void wykonajEksperyment::Wyszukiwanie(unsigned int iloscDanych)
+//{
+//
+//    for(unsigned int i=0; i<iloscDanych;i++)
+//    {
+//        biurko.PUSH(i);
+//    }
+//        for(unsigned int j=0; j<iloscDanych;j++)
+//    {
+//        biurko.POP();
+//    }
+//
+//}
+
+template<class TYP>
+TYP potega(TYP liczba, unsigned int wykladnik)
+ {
+    TYP wynik=1;
+    for(unsigned int i=0; i<wykladnik;i++)
+    {
+        wynik*=liczba;
+    }
+    return wynik;
+ }
+
+//
+//void wykonajEksperyment::wygenerujDaneTestowe()
+//{
+//
+//    ofstream streamDane, streamSprawdzenie;
+//
+//    streamDane.open("dane");
+//    streamSprawdzenie.open("sprawdzenie");
+//
+//    streamDane<<ILOSCDANYCH<<endl;
+//    for(int i=POCZATEK_NUMERACJI;i<POCZATEK_NUMERACJI+ILOSCDANYCH;i++)
+//    {
+//        streamDane<<i<<endl;
+//    }
+//
+//    streamSprawdzenie<<ILOSCDANYCH<<endl;
+//    for(int i=POCZATEK_NUMERACJI;i<POCZATEK_NUMERACJI+ILOSCDANYCH;i++)
+//    {
+//        streamSprawdzenie<<i*2<<endl;
+//    }
+//
+//    streamDane.close();
+//    streamSprawdzenie.close();
+//
+//}
